34b500cb17129ae38b497def24156fffaa423a51
A F <comrade_1997@mail.ru>
2022-12-14 23:53:09 +0300

    '-'

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,6 @@
 
-#ifdef LINX
-#    define CROW_STATIC_DIRECTORY "resourse/"
-#elif defined(WIND)
-#    define CROW_STATIC_DIRECTORY "../working_directory/resourse/"
-#endif
+#define CROW_STATIC_DIRECTORY "resourse/"
+
 #define CROW_STATIC_ENDPOINT "/sus/<path>"
 
 #include "crow/middlewares/cors.h"
