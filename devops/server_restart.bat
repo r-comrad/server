b19123fc3d5f644ae0970637cb0babb816df4aad
A F <comrade_1997@mail.ru>
2022-12-22 00:57:15 +0300

    '-'

--- devops/server_restart.bat
+++ devops/server_restart.bat
@@ -1,6 +1,11 @@
 @echo off
 
-./push_to_git.bat
+cd ..
+git add .
+git add *
+git add -A
+git commit -m '-'
+git push 
 
 cd devops
 plink.exe -batch -ssh rcomrad@194.87.206.68 -pw 111  -m remake.comands -pw 111 > logs.out
