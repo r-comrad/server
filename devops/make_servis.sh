53b4472a147f2912b2949f7cb66de4a56da48d13
A F <comrade_1997@mail.ru>
2022-12-22 01:16:41 +0300

    '-'

--- devops/make_servis.sh
+++ devops/make_servis.sh
@@ -1,7 +1,7 @@
-sudo cp KusManager.service /etc/systemd/system/KusManager.service
-sudo chmod 640 /etc/systemd/system/KusManager.service
-sudo systemctl daemon-reload
-sudo systemctl restart KusManager
+echo -e "111\n" | sudo cp KusManager.service /etc/systemd/system/KusManager.service
+echo -e "111\n" | sudo chmod 640 /etc/systemd/system/KusManager.service
+echo -e "111\n" | sudo systemctl daemon-reload
+echo -e "111\n" | sudo systemctl restart KusManager
 
 # sudo chmod +x ./make_servis.sh
 # sudo ./make_servis.sh
