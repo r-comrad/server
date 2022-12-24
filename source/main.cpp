193fbfb24b9449fc997abc879caedef81c70a9f4
A F <comrade_1997@mail.ru>
2022-12-24 11:54:09 +0300

    '-'

--- source/main.cpp
+++ source/main.cpp
@@ -201,5 +201,15 @@ main()
         return res;
     });
 
+       CROW_ROUTE(app, "/login")
+      .methods("POST"_method)([](const crow::request& req) {
+          auto x = crow::json::load(req.body);
+          if (!x)
+              return crow::response(400);
+
+        crow::json::wvalue res = {"OK"};
+          return crow::response{res};
+      });
+
     app.port(80).multithreaded().run();
 }
